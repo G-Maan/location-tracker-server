0b3f793cc216268c0ed522ff095896b2e7404dd6
galaxiset <galaxiset@gmail.com>
2016-10-19 14:43:20 +0200

    DatabaseConfig

---
+++ src/main/java/com/mielniczuk/DatabaseConfig.java
@@ -0,0 +1,23 @@
+package com.mielniczuk;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by Pawel on 2016-10-19.
+ */
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
