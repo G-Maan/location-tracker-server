22d52fe0fea591a7db2cb323c4a9b5ff6a11f94d
galaxiset <galaxiset@gmail.com>
2016-11-13 19:57:42 +0100

    accept year/month/day hours/minutes date format

--- src/main/java/com/mielniczuk/rest/controller/RestController.java
+++ src/main/java/com/mielniczuk/rest/controller/RestController.java
@@ -115,7 +115,7 @@ public class RestController {
     private Timestamp convertDate(String date){
         try {
             System.out.println("Passed date to method: " + date);
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd HH:mm");
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
             Date parsedDate = simpleDateFormat.parse(date);
             System.out.println("Pre returned date: " + parsedDate);
             Timestamp timestamp = new Timestamp(parsedDate.getTime());
